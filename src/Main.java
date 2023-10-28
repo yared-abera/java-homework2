ce9b34a9eac8d0218bd001a4c638fc5fb1697e5a
yared-abera <yaredoabera7@gmail.com>
2023-10-28 11:10:10 -0700

    this is final modified code that accept input from user

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,55 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-
-
-
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the number of students: ");
+        int numberOfStudents = scanner.nextInt();
+        scanner.nextLine();
 
-        // Creating Student objects
-        Student student1 = new Student("Yaredo", 2506, new int[]{90, 85, 88});
-        Student student2 = new Student("amir", 1574, new int[]{95, 78, 85});
+        System.out.print("Enter the number of teachers: ");
+        int numberOfTeachers = scanner.nextInt();
+        scanner.nextLine();
 
+        Student[] students = new Student[numberOfStudents];
+        Teacher[] teachers = new Teacher[numberOfTeachers];
 
-        // Creating Teacher objects
-        Teacher teacher1 = new Teacher("Ms. Ergoye", "Data commmunication and Network");
-        Teacher teacher2 = new Teacher("Mr. Andamlak", "Data structure and algorithm");
-
-        // Creating College object
         College college = new College("CCI College", new ArrayList<>(), new ArrayList<>());
 
-        // Adding Student and Teacher objects to the College
-        ArrayList<Student> students = college.getStudents();
-        students.add(student1);
-        students.add(student2);
-        college.setStudents(students);
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.println("Enter details for student " + (i + 1) + ":");
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.print("ID: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            System.out.print("Enter 3 grades (space-separated): ");
+            String[] gradesStr = scanner.nextLine().split(" ");
+            int[] grades = new int[3];
+            for (int j = 0; j < 3; j++) {
+                grades[j] = Integer.parseInt(gradesStr[j]);
+            }
+            students[i] = new Student(name, id, grades);
+        }
 
-        ArrayList<Teacher> teachers = college.getTeachers();
-        teachers.add(teacher1);
-        teachers.add(teacher2);
-        college.setTeachers(teachers);
+        for (int i = 0; i < numberOfTeachers; i++) {
+            System.out.println("Enter details for teacher " + (i + 1) + ":");
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.print("Subject: ");
+            String subject = scanner.nextLine();
+            teachers[i] = new Teacher(name, subject);
+        }
 
-        // Print out details
+        college.setStudents(new ArrayList<>(Arrays.asList(students)));
+        college.setTeachers(new ArrayList<>(Arrays.asList(teachers)));
+
         System.out.println("College Name: " + college.getName());
         System.out.println("Students:");
         for (Student student : college.getStudents()) {
-            System.out.println("Name: " + student.getName() + ", ID: " + student.getId() + ", Grades: " + Arrays.toString(student.getGrades()));
+            System.out.2println("Name: " + student.getName() + ", ID: " + student.getId() + ", Grades: " + Arrays.toString(student.getGrades()));
         }
 
         System.out.println("Teachers:");
@@ -42,7 +57,7 @@ public class Main {
             System.out.println("Name: " + teacher.getName() + ", Subject: " + teacher.getSubject());
         }
     }
-    }
+}
 
 
 
